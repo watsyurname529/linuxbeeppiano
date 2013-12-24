61fd5e16ecd6ec88d9b75d882a3fbcbbba06ef10
Finn Ellis <relsqui@chiliahedron.com>
2013-12-23 22:17:09 -0800

    Ported multi-octave feature to the bash script.

--- piano.sh
+++ piano.sh
@@ -1,40 +1,65 @@
 #!/bin/bash
 
 #Thanks to redditor: 'cpbills'
-#for this wonderful bash code
+#for the original wonderful bash code
 
-echo "Welcome to the Linux Beep Piano! (Coded in BASH!)";
-echo "Press keys a-k for whole steps, keys w-u for half steps, and q to exit.";
-echo "Shout out and thank you to redditor 'cpbills' for most of the code!";
+clear
+echo " Welcome to the Linux Beep Piano! (Coded in BASH!)";
+echo " Press keys a-k for white keys, w-e and t-u for black keys, and q to exit.";
+echo " z switches to low octave, c to high octave, x goes back to middle."
+echo " Shout out and thank you to redditor 'cpbills' for most of the code!";
+
+notes=( 261.63 277.18 293.66 311.13 329.63 349.23 369.99 392.00 415.30 440.00 466.16 493.88 523.25 );
+notes_8va=( 523.25 554.37 587.33 622.25 659.26 698.46 739.99 783.99 830.61 880.00 932.33 987.77 1046.50 );
+notes_8vb=( 130.81 138.59 146.83 155.56 164.81 174.61 185.00 196.00 207.65 220.00 233.08 246.94 261.63 );
+
+octave=middle
+
+beep_note() {
+    case "$octave" in
+        low)
+            beep -f ${notes_8vb[$1]};;
+        middle)
+            beep -f ${notes[$1]};;
+        high)
+            beep -f ${notes_8va[$1]};;
+    esac
+}
 
 while read -sn1 key; do
     case "$key" in
         a)
-            beep -f 261.63;; #C4
+            beep_note 0;; #C
         w)
-            beep -f 277.18;; #Db4
+            beep_note 1;; #Db
         s)
-            beep -f 293.66;; #D4
+            beep_note 2;; #D
         e)
-            beep -f 311.13;; #Eb4
+            beep_note 3;; #Eb
         d)
-            beep -f 329.63;; #E4
+            beep_note 4;; #E
         f)
-            beep -f 349.23;; #F4
+            beep_note 5;; #F
         t)
-            beep -f 369.99;; #Gb4
+            beep_note 6;; #Gb
         g)
-            beep -f 392.00;; #G4
+            beep_note 7;; #G
         y)
-            beep -f 415.30;; #Ab4
+            beep_note 8;; #Ab
         h)
-            beep -f 440.00;; #A4
+            beep_note 9;; #A
         u)
-            beep -f 466.16;; #Bb4
+            beep_note 10;; #Bb
         j)
-            beep -f 493.88;; #B4
+            beep_note 11;; #B
         k)
-            beep -f 523.25;; #C5
+            beep_note 12;; #C
+        z)
+            octave=low;;
+        x)
+            octave=middle;;
+        c)
+            octave=high;;
         q)
             break
     esac
